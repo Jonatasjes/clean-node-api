caadfb0914729c10b642ad04b36b142d7da7a69c
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-06-19 15:23:14 -0300

    refactor: returns only object account without a new Promise

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -22,6 +22,6 @@ export class DbAddAccount implements AddAccount {
     const account = await this.addAccountRepository.add(
       Object.assign({}, accountData, { password: hashedPassword })
     )
-    return await new Promise(resolve => resolve(account))
+    return account
   }
 }
