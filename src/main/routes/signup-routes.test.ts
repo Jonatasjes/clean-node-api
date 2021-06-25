ec06006e52e64c808543d50e6f5da82477e5f310
Jonatas Eleoterio <jonatas.eleoterio@outlook.com>
2021-06-24 22:09:48 -0300

    test: ensure Signup route returns an account on success

---
+++ src/main/routes/signup-routes.test.ts
@@ -0,0 +1,17 @@
+import request from 'supertest'
+
+import app from '../config/app'
+
+describe('Signup Routes', () => {
+  test('Should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'Jonatas',
+        email: 'jonatas@mail.com',
+        password: '1234',
+        passwordConfirmation: '1234'
+      })
+      .expect(200)
+  })
+})
